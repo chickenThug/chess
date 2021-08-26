2b242cd7948b84ce0f5d83d466747285c2e859b9
unknown <viggo.svardkrona@gmail.com>
2021-08-26 09:18:45 +0200

    implemented function for generating knight moves

--- move_generator.py
+++ move_generator.py
@@ -2,47 +2,62 @@ from board import Board
 from piece import Piece
 
 
-def generate_moves(board):
+def generate_moves(board, color):
     moves = []
     for i in range(8):
         for j in range(8):
             piece = board.get_piece((i, j))
             type = piece.get_type()
-            color = piece.get_color()
-            if type == 'Pawn':
-                moves += generate_pawn_moves(color, (i, j))
-            elif type == 'Knight':
-                moves += generate_knight_moves(color, (i, j))
-            elif type == 'Bishop':
-                moves += generate_bishop_moves(color, (i, j))
-            elif type == 'Rook':
-                moves += generate_rook_moves(color, (i, j))
-            elif type == 'Queen':
-                moves += generate_queen_moves(color, (i, j))
-            elif type == 'King':
-                moves += generate_king_moves(color, (i, j))
+            piece_color = piece.get_color()
+            if type == 'Pawn' and color == piece_color:
+                moves += generate_pawn_moves(board, color, (i, j))
+            elif type == 'Knight' and color == piece_color:
+                moves += generate_knight_moves(board, color, (i, j))
+            elif type == 'Bishop' and color == piece_color:
+                moves += generate_bishop_moves(board, color, (i, j))
+            elif type == 'Rook' and color == piece_color:
+                moves += generate_rook_moves(board, color, (i, j))
+            elif type == 'Queen' and color == piece_color:
+                moves += generate_queen_moves(board, color, (i, j))
+            elif type == 'King' and color == piece_color:
+                moves += generate_king_moves(board, color, (i, j))
     return moves
 
 
-def generate_pawn_moves(color, coordinate):
+def generate_pawn_moves(board, color, coordinate):
     pass
 
 
-def generate_knight_moves(color, coordinate):
+def generate_knight_moves(board, color, coordinate):
+    moves = []
+    jumps = [(1, 2), (1, -2), (-1, 2), (-1, -2), (2, 1), (2, -1), (-2, 1), (-2, -1)]
+    for jump in jumps:
+        new_x = coordinate[0] + jump[0]
+        new_y = coordinate[1] + jump[1]
+        if in_bounds(new_x, new_y):
+            piece = board.get_piece((new_x, new_y))
+            if piece.get_type() == 'Empty':
+                notation = 'N' + 'abcdefgh'[new_x] + '87654321'[new_y]
+                moves.append(notation)
+            elif not color == piece.get_color():
+                notation = 'Nx' + 'abcdefgh'[new_x] + '87654321'[new_y]
+                moves.append(notation)
+
+
+def generate_bishop_moves(board, color, coordinate):
     pass
 
 
-def generate_bishop_moves(color, coordinate):
+def generate_rook_moves(board, color, coordinate):
     pass
 
 
-def generate_rook_moves(color, coordinate):
+def generate_queen_moves(board, color, coordinate):
     pass
 
 
-def generate_queen_moves(color, coordinate):
+def generate_king_moves(board, color, coordinate):
     pass
 
-
-def generate_king_moves(color, coordinate):
-    pass
+def in_bounds(x,y):
+    return 0 <= x <= 7 and 0 <= y <= 7
